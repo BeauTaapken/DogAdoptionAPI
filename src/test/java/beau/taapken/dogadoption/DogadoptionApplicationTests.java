0b4db0b93aaf4c08915b52fa4151357499e6d611
BeauTaapken <beautaapken@me.com>
2020-02-17 14:06:10 +0100

    * Initial commit

---
+++ src/test/java/beau/taapken/dogadoption/DogadoptionApplicationTests.java
@@ -0,0 +1,13 @@
+package beau.taapken.dogadoption;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogadoptionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
